b027a2eb668fbdf8ae87155a128e708287cf0357
Vadim-Laskin <140655996+Vadim-Laskin@users.noreply.github.com>
2026-08-28 13:20:53 +0700

    Add files via upload

--- netlify/functions/utils/store.js
+++ netlify/functions/utils/store.js
@@ -1,6 +1,15 @@
 const { getStore } = require('@netlify/blobs');
 
 function dataStore() {
+  // On some sites/deploys, Netlify Functions don't automatically receive
+  // the Blobs configuration (a known platform quirk -- shows up as
+  // "MissingBlobsEnvironmentError"). If BLOBS_SITE_ID/BLOBS_TOKEN are set,
+  // we configure the store explicitly instead of relying on auto-detection.
+  const siteID = process.env.BLOBS_SITE_ID;
+  const token = process.env.BLOBS_TOKEN;
+  if (siteID && token) {
+    return getStore({ name: 'site-data', siteID, token });
+  }
   return getStore('site-data');
 }
 
